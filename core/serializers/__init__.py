21a3ab33bfe32fa14803565055c7b0b78085c844
anaclarag <gomes06anaclara@gmail.com>
2024-07-12 14:42:26 -0300

    criação da api cor

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,2 +1,4 @@
 from .user import UserSerializer
 from .acessorios import AcessorioSerializer
+from .categoria import CategoriaSerializer
+from .cor import CorSerializer
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,2 +1,4 @@
 from .user import UserViewSet
 from .acessorios import AcessorioViewSet
+from .categoria import CategoriaViewSet
+from .cor import CorViewSet
